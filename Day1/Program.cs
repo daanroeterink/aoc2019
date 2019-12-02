e0f135cf36dadbb14698f30157a256e227a3ca25
Daan Roeterink <dragon321@gmail.com>
2019-12-02 20:21:28 +0100

    Day 1 added

---
+++ Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+
+        public Program()
+        {
+            var input = File.ReadAllLines("input.txt").Select(int.Parse);
+
+            var sum = input.Sum(GetFuel);
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        public void day1of1()
+        {
+            var input = File.ReadAllLines("input.txt").Select(int.Parse);
+
+            var sum = input.Sum(i => (i / 3) - 2);
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        public int GetFuel(int weight)
+        {
+            var fuel = (weight / 3) - 2;
+
+            if (fuel <= 0)
+            {
+                return 0;
+            }
+
+            return fuel + GetFuel(fuel);
+        }
+    }
+}
