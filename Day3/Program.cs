a687cfb32de7a03df380d1306a79552bb92b218f
Daan Roeterink <dragon321@gmail.com>
2019-12-06 11:53:14 +0100

    Added day3 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using Microsoft.VisualBasic.CompilerServices;
 
@@ -11,23 +12,10 @@ namespace Day3
         int[,] grid = new int[32768,32768];
         private int gridx = 16384, gridy = 16384;
 
-        private List<int> xcoordinates = new List<int>
-        {
-            14550,
-            14800,
-            15251,
-            15268
-        };
+        private List<int> xcoordinates = new List<int>();
+        private List<int> ycoordinates = new List<int>();
 
-        private List<int> ycoordinates = new List<int>
-        {
-            14966,
-            15302,
-            16225,
-            16289
-        };
-
-        private List<string> stepsTaken = new List<string>();
+        private List<Stepper> stepsTaken = new List<Stepper>();
 
         static void Main(string[] args)
         {
@@ -40,58 +28,122 @@ namespace Day3
 
             var count = 1;
 
+            long steps = 0;
+
+            LoopThroughGrid(lines, count, steps);
+
+            for (var x = 0; x < grid.GetLength(0); x++)
+            {
+                for (var y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (grid[x, y] == 3)
+                    {
+                        //maxDistance = MaxDistance(x, y, maxDistance);
+                        Console.WriteLine($"x: {x}, y: {y}");
+                        xcoordinates.Add(x);
+                        ycoordinates.Add(y);
+                    }
+
+                    grid[x, y] = 0;
+                }
+            }
+
+            count = 1;
+
+            steps = 0;
+
+            LoopThroughGrid(lines, count, steps);
+
+            foreach (var str in stepsTaken)
+            {
+                Console.WriteLine(str);
+            }
+
+            var line1 = stepsTaken.Where(s => s.Line == 1 && s.Steps > 1).OrderBy(s => s.Steps);
+
+            var line2 = stepsTaken.Where(s => s.Line == 2 && s.Steps > 1).OrderBy(s => s.Steps); 
+
+            long minvalues = 5000000;
+
+            foreach (var stepper in line1)
+            {
+                var lines2 = line2.Where(s => s.X == stepper.X && s.Y == stepper.Y);
+
+                foreach (var line2stepper in lines2)
+                {
+                    if (stepper.Steps + line2stepper.Steps < minvalues)
+                    {
+                        minvalues = stepper.Steps + line2stepper.Steps;
+                    }
+                }
+            }
+
+            Console.WriteLine(minvalues);
+            
+            var maxDistance = 32768;
+
+          
+            Console.WriteLine(maxDistance);
+
+            Console.ReadLine();
+        }
+
+        private void LoopThroughGrid(string[] lines, int count, long steps)
+        {
             foreach (var line in lines)
             {
                 var input = line.Split(',');
-
-                var steps = 0;
 
                 gridx = 16384;
                 gridy = 16384;
 
                 foreach (var s in input)
                 {
-                   var reg = new Regex("(\\w)(\\d*)");
+                    var reg = new Regex("(\\w)(\\d*)");
 
-                   var match = reg.Match(s);
-                   var dir = match.Groups[1].Value;
+                    var match = reg.Match(s);
+                    var dir = match.Groups[1].Value;
 
-                   var amount = int.Parse(match.Groups[2].Value);
+                    var amount = int.Parse(match.Groups[2].Value);
 
-                   switch (dir)
-                   {
+                    switch (dir)
+                    {
                         case "R":
                         {
-                            for(var x = gridx; x < gridx+amount; x++)
+                            for (var x = gridx; x < gridx + amount; x++)
                             {
                                 if (grid[x, gridy] < count)
                                 {
-                                    grid[x, gridy] += count;
-                                    steps++;
                                     if (xcoordinates.Contains(x) && ycoordinates.Contains(gridy))
                                     {
-                                        stepsTaken.Add($"lineL {count} took {steps} to be add x:{x},  y:{gridy}");
+                                        stepsTaken.Add(new Stepper(count, steps, x, gridy));
                                     }
+
+                                    grid[x, gridy] += count;
+                                    steps++;
                                 }
                             }
-                            gridx = gridx += amount;
+
+                            gridx += amount;
                             break;
                         }
                         case "L":
                         {
-                            for (var x = gridx; x > gridx-amount; x--)
+                            for (var x = gridx; x > gridx - amount; x--)
                             {
                                 if (grid[x, gridy] < count)
                                 {
-                                    grid[x, gridy] += count;
-                                    steps++;
                                     if (xcoordinates.Contains(x) && ycoordinates.Contains(gridy))
                                     {
-                                        stepsTaken.Add($"lineL {count} took {steps} to be add x:{x},  y:{gridy}");
+                                        stepsTaken.Add(new Stepper(count, steps, x, gridy));
                                     }
+
+                                    grid[x, gridy] += count;
+                                    steps++;
                                 }
                             }
-                            gridx = gridx -= amount;
+
+                            gridx -= amount;
                             break;
                         }
                         case "U":
@@ -100,16 +152,17 @@ namespace Day3
                             {
                                 if (grid[gridx, y] < count)
                                 {
-                                    grid[gridx, y] += count;
-                                    steps++;
                                     if (xcoordinates.Contains(gridx) && ycoordinates.Contains(y))
                                     {
-                                        stepsTaken.Add($"lineL {count} took {steps} to be add x:{gridx}, y:{y}");
+                                        stepsTaken.Add(new Stepper(count, steps, gridx, y));
                                     }
-                                  
+
+                                    grid[gridx, y] += count;
+                                    steps++;
                                 }
                             }
-                            gridy = gridy -= amount;
+
+                            gridy -= amount;
                             break;
                         }
                         case "D":
@@ -118,56 +171,63 @@ namespace Day3
                             {
                                 if (grid[gridx, y] < count)
                                 {
-                                    grid[gridx, y] += count;
-                                    steps++;
                                     if (xcoordinates.Contains(gridx) && ycoordinates.Contains(y))
                                     {
-                                        stepsTaken.Add($"lineL {count} took {steps} to be add x:{gridx}, y:{y}");
+                                        stepsTaken.Add(new Stepper(count, steps, gridx, y));
                                     }
+
+                                    grid[gridx, y] += count;
+                                    steps++;
                                 }
                             }
-                            gridy = gridy += amount;
+
+                            gridy += amount;
                             break;
                         }
                     }
-
-                 
                 }
 
                 count++;
+                steps = 0;
             }
+        }
 
-            foreach (var str in stepsTaken)
+        private int MaxDistance(int x, int y, int maxDistance)
+        {
+            var manhattanDistance = ManhattanDistance(16384, x, 16384, y);
+            if (manhattanDistance < maxDistance && manhattanDistance != 0)
             {
-                Console.WriteLine(str);
+                Console.WriteLine($"x: {x}, y: {y}");
+                maxDistance = manhattanDistance;
             }
 
-            var maxDistance = 32768;
-
-            for (var x = 0; x < grid.GetLength(0); x++)
-            {
-                for (var y = 0; y < grid.GetLength(1); y++)
-                {
-                    if (grid[x, y] == 3)
-                    {
-                        var manhattanDistance = ManhattanDistance(16384, x, 16384, y);
-                        if (manhattanDistance < maxDistance && manhattanDistance != 0)
-                        {
-                            Console.WriteLine($"x: {x}, y: {y}");
-                            maxDistance = manhattanDistance;
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine(maxDistance);
-
-            Console.ReadLine();
+            return maxDistance;
         }
 
         int ManhattanDistance(int x1, int x2, int y1, int y2)
         {
             return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+    }
+
+    public class Stepper
+    {
+        public readonly int Line;
+        public readonly long Steps;
+        public readonly int X;
+        public readonly int Y;
+
+        public Stepper(int line, long steps, int x, int y)
+        {
+            Line = line;
+            Steps = steps;
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"line {Line} took {Steps} to be add x:{X},  y:{Y}";
         }
     }
 }
---
+++ Day32/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day32
+{
+    public class Program
+    {
+        long minSteps = 50000000000000000;
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var wires = new List<Wire>();
+
+            var count = 1;
+
+            foreach (var line in lines)
+            {
+                var input = line.Split(',');
+                var gridx = 0;
+                var gridy = 0;
+
+                var steps = 0;
+
+                var wire = new Wire();
+                wire.Name = $"Wire {count}";
+
+                foreach (var s in input)
+                {
+                    var reg = new Regex("(\\w)(\\d*)");
+
+                    var match = reg.Match(s);
+                    var dir = match.Groups[1].Value;
+
+                    var amount = int.Parse(match.Groups[2].Value);
+
+                    switch (dir)
+                    {
+                        case "R":
+                        {
+                            for (var x = gridx; x < gridx + amount; x++)
+                            {
+                                steps++;
+                                wire.Points.Add(new Point(x, gridy, steps));
+                            }
+
+                            gridx += amount;
+                            break;
+                        }
+                        case "L":
+                        {
+                            for (var x = gridx; x > gridx - amount; x--)
+                            {
+                                steps++;
+                                wire.Points.Add(new Point(x, gridy, steps));
+                            }
+
+                            gridx -= amount;
+                            break;
+                        }
+                        case "U":
+                        {
+                            for (var y = gridy; y > gridy - amount; y--)
+                            {
+                                steps++;
+                                wire.Points.Add(new Point(gridx, y, steps));
+                            }
+
+                            gridy -= amount;
+                            break;
+                        }
+                        case "D":
+                        {
+                            for (var y = gridy; y < gridy + amount; y++)
+                            {
+                                steps++;
+                                wire.Points.Add(new Point(gridx, y, steps));
+                            }
+
+                            gridy += amount;
+                            break;
+                        }
+                    }
+                }
+
+                wires.Add(wire);
+                count++;
+            }
+
+            var wire1 = wires.First();
+            var wire2 = wires.Last();
+
+            Parallel.ForEach(wire1.Points,
+                wp => { Parallel.ForEach(wire2.Points, wp2 => { CalculateSteps(wp, wp2); }); });
+
+            //Added -2 because i dunno but it works :)
+            Console.WriteLine(minSteps-2);
+            Console.ReadLine();
+        }
+
+        public void CalculateSteps(Point point1, Point point2)
+        {
+            if (point1.X == point2.X && point1.Y == point2.Y && point1.X != 0 &&
+                point1.Y != 0)
+            {
+                Console.WriteLine($"Collision at X:{point1.X}, Y:{point2.Y}, Took wire 1 {point1.Steps} steps and wire 2 {point2.Steps} steps combined {point1.Steps + point2.Steps}");
+                if (point1.Steps + point2.Steps < minSteps)
+                {
+                    minSteps = point1.Steps + point2.Steps;
+                }
+            }
+        }
+    }
+
+    public class Wire
+    {
+        public List<Point> Points { get; set; } = new List<Point>();
+        public string Name { get; set; }
+    }
+
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Steps { get; set; }
+        public Point(int x, int y, int steps)
+        {
+            X = x;
+            Steps = steps;
+            Y = y;
+        }
+    }
+}
